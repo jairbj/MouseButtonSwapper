5f21f1ceeeca33a683863e1ddf8a34a3fe87b384
Jair Junior <1747830+jairbj@users.noreply.github.com>
2023-04-10 18:25:44 -0300

    Add an optional argument for selecting with button will be the main.

--- MouseButtonSwapper.cpp
+++ MouseButtonSwapper.cpp
@@ -3,13 +3,33 @@
 
 #include <Windows.h>
 
-int main(){
-    // This set mouse button reversed but also returns 1 if it was
-    // already reversed.
-    if (SwapMouseButton(1) != 0)
+int main(int argc, char* argv[]) {
+    if (argc == 2)
     {
-        // If it was already reversed, make it normal again
-        SwapMouseButton(0);
+        if (strcmp(argv[1],"left"))
+        {
+            //Set main mouse button to left
+            SwapMouseButton(0);
+        }
+        else if (strcmp(argv[1], "right"))
+        {
+            //Set main mouse button to right
+            SwapMouseButton(1);
+        }
+        else 
+        {
+            return 999;
+        }
+    } 
+    else 
+    {
+        // This set mouse button reversed but also returns 1 if it was
+        // already reversed.
+        if (SwapMouseButton(1) != 0)
+        {
+            // If it was already reversed, make it normal again
+            SwapMouseButton(0);
+        }
     }
     return 0;
 }
